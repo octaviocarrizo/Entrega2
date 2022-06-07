0528bfa563d1883d2156d84bda1a48cbb56a039e
octaviocarrizo <oxtosito2.0@gmail.com>
2022-06-07 01:14:02 -0300

    Entrega2

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+export default function NavBar() { 
+    return(
+     <nav>
+        <ul>
+            <li>Ilustraciones</li>
+            <li>Hot ilustraciones</li>
+            <li>ofertas</li>
+        </ul>
+    </nav>
+    )
+
+}
